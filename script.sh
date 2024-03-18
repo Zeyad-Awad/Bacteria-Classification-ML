304b6943796244592bdaf6439219205a347ab5c6
Zeyad Awad <zeyadahmed@me.com>
2024-03-18 01:42:03 -0300

    downloaded dataset

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-cd data/s__Campylobacter_jejuni
+cd new_data/Yersinia_enterocolitica
 rm -r 
-curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_001457695.1/download?include_annotation_type=GENOME_FASTA&filename=GCF_001457695.1.zip" -H "Accept: application/zip"
+curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_002082245.2/download?include_annotation_type=GENOME_FASTA&filename=GCF_002082245.2.zip" -H "Accept: application/zip"
 echo "Extracting and cleaning ... "
 unzip -o '*.zip'
 rm *.zip
