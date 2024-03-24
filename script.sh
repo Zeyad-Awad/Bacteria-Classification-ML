8006b948dc01a6fd6b070f85346428bccd0ee1e8
Zeyad Awad <zeyadahmed@me.com>
2024-03-24 14:38:36 -0300

    phase1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-cd patho_dataset/Yersinia_enterocolitica
+cd non_dataset/Enterococcus_faecalis
 rm -r 
-curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_002082245.2/download?include_annotation_type=GENOME_FASTA&filename=GCF_002082245.2.zip" -H "Accept: application/zip"
+curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCA_000415185.1/download?include_annotation_type=GENOME_FASTA&filename=GCA_000415185.1.zip" -H "Accept: application/zip"
 echo "Extracting and cleaning ... "
 unzip -o '*.zip'
 rm *.zip
